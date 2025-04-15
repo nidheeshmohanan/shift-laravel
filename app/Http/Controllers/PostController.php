d7a21d417a8cf813e93ad9c9fcdb4245f265f068
Nidheesh <nidheesh.mohanan@cubettech.com>
2025-04-15 15:57:29 +0530

    Post and User crud

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\User;
+use Illuminate\Http\Request;
+use Auth;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with('user')->latest()->paginate(10);
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'   => 'required|string|max:255',
+            'content' => 'required',
+        ]);
+
+        Post::create([
+            'title'   => $request->title,
+            'content' => $request->content,
+            'user_id' => Auth::id(),
+        ]);
+
+        return redirect()->route('posts.index')->with('success', 'Post created!');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title'   => 'required|string|max:255',
+            'content' => 'required',
+        ]);
+
+        $post->update($request->only('title', 'content'));
+
+        return redirect()->route('posts.index')->with('success', 'Post updated!');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('success', 'Post deleted!');
+    }
+}
